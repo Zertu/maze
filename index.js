246994b416946f1604155bf8440a555b649909e8
zertu <290055513@qq.com>
2017-04-05 21:40:52 +0800

    第一题

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ window.onload=()=>{
    maze.fillStyle='#d5d5d5'
    maze.fillRect(0,0,500,500)
    drawgrid(maze,20)
+   drawmaze(maze,20)
 }
 
 
 
+
+/**
+ * 绘制迷宫
+ * 
+ * @param {any} maze 
+ */
+function drawmaze(maze,interval){
+    
+    for(let i =1;i<maze.canvas.width;i+=interval){
+        
+    }
+}
+
+/**
+ * 画迷宫的格子，方便生成迷宫
+ * 
+ * @param {CanvasObject} maze 
+ * @param {Number} interval 
+ */
 function drawgrid(maze,interval){
     for(let i=1;i<maze.canvas.width;i+=interval){
         maze.moveTo(i,0)
